86ceaa4907ac47c0363c2f56106bc0101eb0077b
Sean Campbell <sean.campbell13@gmail.com>
2024-08-20 19:49:50 -0400

    before powering on the machine

--- internal/compiler/compiler.go
+++ internal/compiler/compiler.go
@@ -19,7 +19,55 @@ func New() *Compiler {
 }
 
 func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+
+	case *ast.IntegerLiteral:
+		intObj := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(intObj))
+	}
+
 	return nil
+}
+
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+func (c *Compiler) emit(op code.Opcode, operands ...int) int {
+	inst := code.Make(op, operands...)
+	pos := c.addInstruction(inst)
+	return pos
+}
+
+func (c *Compiler) addInstruction(ins []byte) int {
+	posNewInstruction := len(c.instructions)
+	c.instructions = append(c.instructions, ins...)
+	return posNewInstruction
 }
 
 func (c *Compiler) ByteCode() *ByteCode {
