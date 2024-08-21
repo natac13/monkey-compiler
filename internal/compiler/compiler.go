56ddc32878196557534b659071d040c98c0577f3
Sean Campbell <sean.campbell13@gmail.com>
2024-08-21 19:58:27 -0400

    up to hook up the repl

--- internal/compiler/compiler.go
+++ internal/compiler/compiler.go
@@ -1,6 +1,8 @@
 package compiler
 
 import (
+	"fmt"
+
 	"github.com/natac13/monkey-compiler/internal/ast"
 	"github.com/natac13/monkey-compiler/internal/code"
 	"github.com/natac13/monkey-compiler/internal/object"
@@ -43,6 +45,13 @@ func (c *Compiler) Compile(node ast.Node) error {
 		err = c.Compile(node.Right)
 		if err != nil {
 			return err
+		}
+
+		switch node.Operator {
+		case "+":
+			c.emit(code.OpAdd)
+		default:
+			return fmt.Errorf("unknown operator %s", node.Operator)
 		}
 
 	case *ast.IntegerLiteral:
